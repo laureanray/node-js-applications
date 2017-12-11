fb1d793916f50dff657d61293e69c2b611135587
Laurean Ray Bahala <laureanraybahala@gmail.com>
2017-12-11 22:01:18 +0800

    Adding files.

---
+++ notes-app/app.js
@@ -0,0 +1,14 @@
+console.log('Starting app.js');
+
+const fs = require('fs');
+const os = require('os');
+const notes = require('./notes.js');
+
+
+console.log('Result: ', notes.add(9,-2));
+// var res = notes.addNote();
+// console.log(res);
+// var user = os.userInfo();
+//
+//
+// fs.appendFileSync('greetings.txt', `Hello ${user.username}! You are ${notes.age}.`);
---
+++ notes-app/notes.js
@@ -0,0 +1,10 @@
+console.log('Staring notes.js');
+
+module.exports.addNote = () => {
+  console.log('addNote');
+  return 'New Note';
+};
+
+module.exports.add = (a, b) => {
+  return a + b;
+};
